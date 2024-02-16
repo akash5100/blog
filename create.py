a93fdb0da44c94b481e61f601e9b3c82a12bb8c8
akash5100 <shivamvermashs@gmail.com>
2024-02-16 21:59:33 +0530

    GRU

--- create.py
+++ create.py
@@ -8,15 +8,15 @@ def handle_spaces(x: str) -> str:
 
 
 def script():
-    category = ""
+    tags = ""
     title = datetime.date.today().strftime("%Y-%m-%d") + "-"
     if len(sys.argv) <= 1:
-        print('usage [-t "title"] [-c "category"] ')
+        print('usage [-t "title"] [-g "tags"] ')
         sys.exit(1)
 
     for i in range(len(sys.argv)):
-        if sys.argv[i] == "-c":
-            category = sys.argv[i+1]
+        if sys.argv[i] == "-g":
+            tags = sys.argv[i+1]
         elif sys.argv[i] == "-t":
             x = sys.argv[i+1]
 
@@ -28,10 +28,10 @@ def script():
     with open(file_path, "w") as file:
         file.write("---\n")
         file.write(f"title: {x}\n")
-        file.write(f"categories: {category}\n")
+        file.write(f"tags: {tags}\n")
         file.write("---\n\n")
     file.close()
-    print(f"Created\n> @'{file_path}' \n> category: '{category}'")
+    print(f"Created\n> @'{file_path}' \n> tags: '{tags}'")
 
 
 if __name__ == '__main__':
